53e541b61274a0b4112af4c6d18b90a737ec17a5
MatVit2910 <mateoviteri13579@gmail.com>
2025-03-13 14:07:09 -0500

    Change in Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,12 +1,28 @@
 import { useGSAP } from "@gsap/react";
 import { animateWithGsap } from "../utils/animations";
-import { exploreVideo } from "../utils";
+import { explore1Img, explore2Img, exploreVideo } from "../utils";
 import { useRef } from "react";
+import gsap from "gsap";
 
 const Features = () => {
   const videoRef = useRef();
   useGSAP(() => {
+    gsap.to("exploreVideo", {
+      scrollTrigger: {
+        trigger: "exploreVideo",
+        toggleActions: "restart reverse restart reverse",
+        start: "top 85%",
+        ...scrollProps,
+      },
+    });
+    animateWithGsap("#exploreVideo");
     animateWithGsap("#features_title", { y: 0, opacity: 1 });
+    animateWithGsap(
+      ".g_grow",
+      { scale: 1, opacity: 1, ease: "power1" },
+      { scrub: 5.5 }
+    );
+    animateWithGsap(".g_text", { y: 0, opacity: 1, ease: "power2.inOut" });
   }, []);
 
   return (
@@ -37,6 +53,46 @@ const Features = () => {
                 src={exploreVideo}
                 type="video/mp4"
               />
+            </div>
+            <div className="flex flex-col w-full relative">
+              <div className="feature-video-container">
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                  <img
+                    className="feature-video g_grow"
+                    src={explore1Img}
+                    alt="titanium"
+                  />
+                </div>
+                <div className="overflow-hidden flex-1 h-[50vh]">
+                  <img
+                    className="feature-video g_grow"
+                    src={explore2Img}
+                    alt="titanium2"
+                  />
+                </div>
+              </div>
+              <div className="feature-text-container">
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    iPhone 15 Pro is{" "}
+                    <span className="text-white">
+                      the first iPhone to feature an aerospace-grade titanium
+                      design using the same alloy that spacecrafts use for
+                      missions to Mars
+                    </span>
+                  </p>
+                </div>
+                <div className="flex-1 flex-center">
+                  <p className="feature-text g_text">
+                    Titanium has one of the best strength-to-weight ratios of
+                    any metal, making these our{" "}
+                    <span className="text-white">
+                      lightest Pro models ever.
+                    </span>
+                    You'll notice the difference the moment you pick one up.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -5,7 +5,7 @@ gsap.registerPlugin(ScrollTrigger);
 export const animateWithGsap = (target, animationProps, scrollProps) => {
   gsap.to(target, {
     ...animationProps,
-    ScrollTrigger: {
+    scrollTrigger: {
       trigger: target,
       toggleActions: "restart reverse restart reverse",
       start: "top 85%",
